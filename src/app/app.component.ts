b5b346ad62651eec198698f064630bb57c60b241
ritikakri0312 <ritikakri0312@gmail.com>
2023-07-13 23:36:01 +0530

    bhai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rg';
+  // title = 'hiibii';
+  data = "code step by step";
+  // getValue()
+  // {
+  //   return "get functon data"
+  // }
+  getData(val: string) {
+    console.warn(val)
+  }
+  displayVal = '';
+  getValue(val: string) {
+    console.warn(val)
+    this.displayVal = val
+  }
+  count = 0
+  counter(type: string) {
+    type == 'add' ? this.count++ : this.count--
+  }
+
+  name = "peter"
+  disable = false;
+
+  show = "yes"
+  color = "blue"
+  color2 = "orangered";
+  users = ['ánil', 'bhashkar', 'sam', 'peter', 'burse'];
+  userDetails = [
+    { name: 'Anil', email: 'anil@test.com', phone: '88888' },
+    { name: 'Bhashkar', email: 'bhashkar@test.com', phone: '11111' },
+    { name: 'Sam', email: 'sam@test.com', phone: '22222' },
+    { name: 'peter', email: 'peter@test.com', phone: 'peter' },
+  ];
+  usersnested = [
+    { name: 'Anil', phone: '111111', socialAccounts: ['facebook', 'insta', 'gmail'] },
+    { name: 'Sam', phone: '222222', socialAccounts: ['youtube', 'insta', 'gmail'] },
+    { name: 'Peter', phone: '333333', socialAccounts: ['yahoo', 'facebook', 'gmail'] },
+    { name: 'bruice', phone: '444444', socialAccounts: ['linked', 'insta', 'gmail'] },
+  ]
+
+  title = 'style Binding';
+  colorBinding = "red";
+  bgColor = 'green';
+
+  updateColor(){
+    this.colorBinding = "blue"
+    this.bgColor = "orange"
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
+
+
+import { UserListComponent } from './user-list/user-list.component';
+
+
+import { StudentListComponent } from './student-list/student-list.component';
+import { CountryListComponent } from './country-list/country-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+    
+    UserListComponent,
+    
+
+    StudentListComponent,
+    CountryListComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
