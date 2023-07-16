b72280b271598267371a955cd04d6c41b87f20c2
ritikakri0312 <ritikakri0312@gmail.com>
2023-07-16 18:41:08 +0530

    hjkl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,63 +6,67 @@ import {NgForm} from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'hiibii';
-  data = "code step by step";
-  // getValue()
-  // {
-  //   return "get functon data"
+  // // title = 'hiibii';
+  // data = "code step by step";
+  // // getValue()
+  // // {
+  // //   return "get functon data"
+  // // }
+  // getData(val: string) {
+  //   console.warn(val)
   // }
-  getData(val: string) {
-    console.warn(val)
-  }
-  displayVal = '';
-  getValue(val: string) {
-    console.warn(val)
-    this.displayVal = val
-  }
-  count = 0
-  counter(type: string) {
-    type == 'add' ? this.count++ : this.count--
-  }
+  // displayVal = '';
+  // getValue(val: string) {
+  //   console.warn(val)
+  //   this.displayVal = val
+  // }
+  // count = 0
+  // counter(type: string) {
+  //   type == 'add' ? this.count++ : this.count--
+  // }
 
-  name = "petewa"
-  disable = false;
+  // name = "petewa"
+  // disable = false;
 
-  show = "yes"
-  color = "blue"
-  color2 = "orangered";
-  users = ['ánil', 'bhashkar', 'sam', 'peter', 'burse'];
-  userDetails = [
-    { name: 'Anil', email: 'anil@test.com', phone: '88888' },
-    { name: 'Bhashkar', email: 'bhashkar@test.com', phone: '11111' },
-    { name: 'Sam', email: 'sam@test.com', phone: '22222' },
-    { name: 'peter', email: 'peter@test.com', phone: 'peter' },
-  ];
-  usersnested = [
-    { name: 'Anil', phone: '111111', socialAccounts: ['facebook', 'insta', 'gmail'] },
-    { name: 'Sam', phone: '222222', socialAccounts: ['youtube', 'insta', 'gmail'] },
-    { name: 'Peter', phone: '333333', socialAccounts: ['yahoo', 'facebook', 'gmail'] },
-    { name: 'bruice', phone: '444444', socialAccounts: ['linked', 'insta', 'gmail'] },
-  ]
+  // show = "yes"
+  // color = "blue"
+  // color2 = "orangered";
+  // users = ['ánil', 'bhashkar', 'sam', 'peter', 'burse'];
+  // userDetails = [
+  //   { name: 'Anil', email: 'anil@test.com', phone: '88888' },
+  //   { name: 'Bhashkar', email: 'bhashkar@test.com', phone: '11111' },
+  //   { name: 'Sam', email: 'sam@test.com', phone: '22222' },
+  //   { name: 'peter', email: 'peter@test.com', phone: 'peter' },
+  // ];
+  // usersnested = [
+  //   { name: 'Anil', phone: '111111', socialAccounts: ['facebook', 'insta', 'gmail'] },
+  //   { name: 'Sam', phone: '222222', socialAccounts: ['youtube', 'insta', 'gmail'] },
+  //   { name: 'Peter', phone: '333333', socialAccounts: ['yahoo', 'facebook', 'gmail'] },
+  //   { name: 'bruice', phone: '444444', socialAccounts: ['linked', 'insta', 'gmail'] },
+  // ]
 
-  title = 'style Binding';
-  colorBinding = "red";
-  bgColor = 'green';
+  // title = 'style Binding';
+  // colorBinding = "red";
+  // bgColor = 'green';
 
-  updateColor(){
-    this.colorBinding = "blue"
-    this.bgColor = "orange"
-  }
+  // updateColor(){
+  //   this.colorBinding = "blue"
+  //   this.bgColor = "orange"
+  // }
   
-  data23: any = {};
-  getData23(data:NgForm){
-    console.warn(data)
-    this.data23 = data
+  // data23: any = {};
+  // getData23(data:NgForm){
+  //   console.warn(data)
+  //   this.data23 = data
 
 
-  }
+  // }
 
 
-
+title= 'Toggle Element';
+display = true
+toggle(){
+   this.display=!this.display;
+}
 
 }
